633220f842d7a318f4fd7764a08bc36b4ef2843c
Shreenibas Samal <shreenibassamal@gmail.com>
2024-12-08 17:57:06 +0530

    all arithimatic operation

---
+++ 01_basics/05_arithimaticOpertation.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version
+// Write a java prpgram  for all arithimatic operation.
+let a = 100;
+let b = 10;
+let addition = a+b;
+let substraction = a-b;
+let multiplication = a*b;
+let modulus = a%b;
+let division = a/b;
+console.log(addition);
+console.log(substraction);
+console.log(multiplication);
+console.log(modulus);
+console.log(division);
