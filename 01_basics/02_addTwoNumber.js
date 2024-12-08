f907f8ea255506c93b31693409066087225242a3
Shreenibas Samal <shreenibassamal@gmail.com>
2024-12-08 17:23:45 +0530

    Basic arithimatic operation

---
+++ 01_basics/02_addTwoNumber.js
@@ -0,0 +1,4 @@
+// write a JS program to add two number
+let a = 10;
+let b = 20;
+console.log("addition of two numbwr is: "+ (a+b));
---
+++ 01_basics/03_substratTwoNumber.js
@@ -0,0 +1,4 @@
+// Write a java prpgram to substract two numnber.
+let a = 10;
+let b = 11;
+console.log("substract of two number is: "+(a+b));
